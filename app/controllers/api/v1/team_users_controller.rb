b3e2180ec27da92db5c36295b902b6116252d04f
dongqilai <85636682@qq.com>
2016-06-29 14:39:51 +0800

    current_user

--- app/controllers/api/v1/team_users_controller.rb
+++ app/controllers/api/v1/team_users_controller.rb
@@ -2,6 +2,7 @@ class Api::V1::TeamUsersController < Api::V1::BaseController
   before_action :verify_auth_token, only: [:create, :destroy]
 
   def create
+    Rails.logger.error(current_user)
     @team_user = TeamUser.new(user_id: current_user.id, team_id: params[:team_id])
     if not @team_user.save
       api_error(400)
