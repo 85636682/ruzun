b41c5ce05d03152341020fbf15847bcb9c2a9705
dongqilai <85636682@qq.com>
2016-06-29 14:22:36 +0800

    关联队伍和球员

--- app/controllers/api/v1/team_users_controller.rb
+++ app/controllers/api/v1/team_users_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::TeamUsersController < Api::V1::BaseController
   before_action :verify_auth_token, only: [:create, :destroy]
 
   def create
-    @team_user = TeamUser.new(user_id: @current_user.id, team_id: params[:team_id])
+    @team_user = TeamUser.new(user_id: current_user.id, team_id: params[:team_id])
     if not @team_user.save
       api_error(400)
     end
