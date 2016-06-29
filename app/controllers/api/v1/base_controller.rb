ee9675746bfc2773d90b693df0b7a5c2977adabe
dongqilai <85636682@qq.com>
2016-06-29 14:32:31 +0800

    current_user

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -9,6 +9,8 @@ class Api::V1::BaseController < ApplicationController
   # disable the CSRF token
   skip_before_action :verify_authenticity_token
 
+  attr_reader :current_user
+
   def destroy_session
     request.session_options[:skip] = true
   end
