1272d8179bc7493017bbdfa5a6ed2368cb356d74
dongqilai <85636682@qq.com>
2016-06-30 12:25:08 +0800

    user avatar

---
+++ app/uploaders/user_avatar_uploader.rb
@@ -0,0 +1,13 @@
+# encoding: utf-8
+
+class TeamLogoUploader < BaseUploader
+
+  def default_url
+    "#{Setting.upyun_upload_url}/user_default_avatar.png#{version_name}"
+  end
+
+  def extension_white_list
+    %w(jpg jpeg gif png svg)
+  end
+
+end
