b6179cc6e7f32985034f37e848d02a78e3276371
dongqilai <85636682@qq.com>
2016-07-01 19:14:42 +0800

    擅长位置

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ActiveRecord::Base
   has_secure_password
 
+  extend Enumerize  #pending 新建状态  cooking烹饪状态  finished完成状态  canceled取消
+  enumerize :position,     in: [:PG, :SG, :SF, :PF, :C], default: :SF
+
   mount_uploader :avatar, UserAvatarUploader
   attr_accessor :uploader_secure_token
 
