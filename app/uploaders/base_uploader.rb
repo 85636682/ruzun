954d700c3a8243733288ac55585ce80e97c7af5d
dongqilai <85636682@qq.com>
2016-07-03 11:36:21 +0800

    头像代码更新

--- app/uploaders/base_uploader.rb
+++ app/uploaders/base_uploader.rb
@@ -1,32 +1,17 @@
-IMAGE_UPLOADER_ALLOW_IMAGE_VERSION_NAMES = %(80x80 300x300 500x500 800x800 320xAuto 520xAuto 820xAuto)
+# IMAGE_UPLOADER_ALLOW_IMAGE_VERSION_NAMES = %(80x80 300x300 500x500 800x800 320xAuto 520xAuto 820xAuto)
+require 'carrierwave/processing/mini_magick'
 class BaseUploader < CarrierWave::Uploader::Base
+  include CarrierWave::MiniMagick
+
   def store_dir
-    "#{model.class.to_s.underscore}/#{mounted_as}"
+    model.class.to_s.underscore
   end
 
   def default_url
     "#{Setting.upyun_upload_url}/anonymousUser.jpg#{version_name}"
   end
 
-  def url(version_name = "")
-    @url ||= super({})
-    version_name = version_name.to_s
-    return @url if version_name.blank?
-    if not version_name.in?(IMAGE_UPLOADER_ALLOW_IMAGE_VERSION_NAMES)
-      raise "BaseUploader version_name:#{version_name} not allow."
-    end
-    [@url, version_name].join("!")
-  end
-
-  def ext
-    file.extension.downcase
-  end
-
-  def filename
-    if super.present?
-      model.uploader_secure_token ||= SecureRandom.uuid.gsub("-", "")
-      Rails.logger.debug("(BaseUploader.filename) #{model.uploader_secure_token}")
-      "#{model.uploader_secure_token}.#{ext}"
-    end
+  def extension_white_list
+    %w(jpg jpeg png gif)
   end
 end
--- app/uploaders/user_avatar_uploader.rb
+++ app/uploaders/user_avatar_uploader.rb
@@ -2,12 +2,34 @@
 
 class UserAvatarUploader < BaseUploader
 
+  version :normal do
+    process resize_to_fill: [48, 48]
+  end
+
+  version :small do
+    process resize_to_fill: [16, 16]
+  end
+
+  version :large do
+    process resize_to_fill: [96, 96]
+  end
+
+  version :big do
+    process resize_to_fill: [120, 120]
+  end
+
+  def filename
+    if super.present?
+      "avatar/#{model.id}.jpg"
+    end
+  end
+
   def default_url
     "#{Setting.upyun_upload_url}/user_default_avatar.png#{version_name}"
   end
 
   def extension_white_list
-    %w(jpg jpeg gif png svg)
+    %w(jpg jpeg png)
   end
 
 end
