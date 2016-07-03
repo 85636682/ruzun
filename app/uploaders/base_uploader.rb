780f25a23fff4c1a0fcdcd94acbf31138281250f
dongqilai <85636682@qq.com>
2016-07-03 11:40:29 +0800

    头像代码更新

--- app/uploaders/base_uploader.rb
+++ app/uploaders/base_uploader.rb
@@ -1,17 +1,32 @@
-# IMAGE_UPLOADER_ALLOW_IMAGE_VERSION_NAMES = %(80x80 300x300 500x500 800x800 320xAuto 520xAuto 820xAuto)
-require 'carrierwave/processing/mini_magick'
+IMAGE_UPLOADER_ALLOW_IMAGE_VERSION_NAMES = %(80x80 300x300 500x500 800x800 320xAuto 520xAuto 820xAuto)
 class BaseUploader < CarrierWave::Uploader::Base
-  include CarrierWave::MiniMagick
-
   def store_dir
-    model.class.to_s.underscore
+    "#{model.class.to_s.underscore}/#{mounted_as}"
   end
 
   def default_url
     "#{Setting.upyun_upload_url}/anonymousUser.jpg#{version_name}"
   end
 
-  def extension_white_list
-    %w(jpg jpeg png gif)
+  def url(version_name = "")
+    @url ||= super({})
+    version_name = version_name.to_s
+    return @url if version_name.blank?
+    if not version_name.in?(IMAGE_UPLOADER_ALLOW_IMAGE_VERSION_NAMES)
+      raise "BaseUploader version_name:#{version_name} not allow."
+    end
+    [@url, version_name].join("!")
+  end
+
+  def ext
+    file.extension.downcase
+  end
+
+  def filename
+    if super.present?
+      model.uploader_secure_token ||= SecureRandom.uuid.gsub("-", "")
+      Rails.logger.debug("(BaseUploader.filename) #{model.uploader_secure_token}")
+      "#{model.uploader_secure_token}.#{ext}"
+    end
   end
 end
