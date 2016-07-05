eb40bef3c942977e500762ff838196c96623c9c0
dongqilai <85636682@qq.com>
2016-07-05 18:01:30 +0800

    微信js

---
+++ app/controllers/api/v1/wechat_controller.rb
@@ -0,0 +1,7 @@
+class Api::V1::WechatController < Api::V1::BaseController
+  def sign_package
+    @wechat_client ||= WeixinAuthorize::Client.new(Rails.application.secrets.wechat_app_id, Rails.application.secrets.wechat_app_secret)
+    sign_package = @wechat_client.get_jssign_package(params[:request_url])
+    render sign_package
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
+      get 'wechat/sign_package'
+      
       resources :users, only: [:create, :show, :update]
       resources :teams, only: [:index, :create, :show, :update]
       resources :team_users, only: [:create, :destroy]
