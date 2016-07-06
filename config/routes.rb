c493baa21fc1b9e6a614b5ea65429cf81640bda8
dongqilai <85636682@qq.com>
2016-07-06 13:18:50 +0800

    微信上传图片

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       get 'wechat/sign_package'
-      
+      get 'wechat/get_image_from_wechat'
+
       resources :users, only: [:create, :show, :update]
       resources :teams, only: [:index, :create, :show, :update]
       resources :team_users, only: [:create, :destroy]
