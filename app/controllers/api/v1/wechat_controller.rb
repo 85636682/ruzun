9d672f9c169d7030428cbfaf053e4c263778d92c
dongqilai <85636682@qq.com>
2016-07-06 10:31:04 +0800

    微信js

--- app/controllers/api/v1/wechat_controller.rb
+++ app/controllers/api/v1/wechat_controller.rb
@@ -1,5 +1,6 @@
 class Api::V1::WechatController < Api::V1::BaseController
   def sign_package
+    Rails.logger.info(params)
     Rails.logger.info(params[:request_url])
     @wechat_client ||= WeixinAuthorize::Client.new(Rails.application.secrets.wechat_app_id, Rails.application.secrets.wechat_app_secret)
     sign_package = @wechat_client.get_jssign_package(params[:request_url])
