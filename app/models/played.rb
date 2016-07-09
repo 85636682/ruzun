4d1222e7924ab713799a1ad5221371acbf348905
dongqilai <85636682@qq.com>
2016-07-09 14:34:27 +0800

    场次

--- app/models/played.rb
+++ app/models/played.rb
@@ -1,6 +1,6 @@
 class Played < ActiveRecord::Base
   belongs_to :game
   has_many :techstats
-  has_one :home_team, class_name: "Team", foreign_key: home_team_id
-  has_one :guest_team, class_name: "Team", foreign_key: guest_team_id
+  has_one :home_team, class_name: "Team", foreign_key: "home_team_id"
+  has_one :guest_team, class_name: "Team", foreign_key: "guest_team_id"
 end
