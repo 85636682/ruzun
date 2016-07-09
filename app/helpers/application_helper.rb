f7c0f59a85df314b9ae523cc571d66389993777b
dongqilai <85636682@qq.com>
2016-07-09 15:45:01 +0800

    场次

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,23 @@ module ApplicationHelper
   def sanitize_markdown(body)
     sanitize(body, tags: ALLOW_TAGS, attributes: ALLOW_ATTRIBUTES)
   end
+
+  def formattime(time, options = {})
+    options[:unit]
+    options[:unit] = options[:unit].blank? ? "day" : options[:unit]
+    case options[:unit]
+    when "day"
+      DateTime.parse(time.iso8601).strftime('%Y年%m月%d日') if time
+    when "noYearOnlyDay"
+      DateTime.parse(time.iso8601).strftime('%m月%d日') if time
+    when "noYear"
+      DateTime.parse(time.iso8601).strftime('%m月%d日 %H:%M') if time
+    when "hour"
+      DateTime.parse(time.iso8601).strftime('%Y年%m月%d日 %H:%M') if time
+    when "noDayOnlyHour"
+      DateTime.parse(time.iso8601).strftime('%H:%M') if time
+    when "numberString"
+      DateTime.parse(time.iso8601).strftime('%Y%m%d%H%M') if time
+    end
+  end
 end
