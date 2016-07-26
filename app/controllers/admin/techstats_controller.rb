d1e79a112d2d1be3791f4a622818562946892de2
dongqilai <85636682@qq.com>
2016-07-26 08:30:03 +0800

    一对多

--- app/controllers/admin/techstats_controller.rb
+++ app/controllers/admin/techstats_controller.rb
@@ -4,7 +4,7 @@ class Admin::TechstatsController < ApplicationController
   before_action :set_techstat, only: [:edit, :update]
 
   def index
-    @techstats = @played.techstats.joins(:user).group_by{ |t| t.team_id }
+    @techstats = @played.techstats.group_by{ |t| t.user.team_id }
   end
 
   def create
