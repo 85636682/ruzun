2c88743e88d9b35e822f173356acdfd8df3a1d6a
dongqilai <85636682@qq.com>
2016-07-27 20:30:02 +0800

    upload

--- app/controllers/api/v1/wechat_controller.rb
+++ app/controllers/api/v1/wechat_controller.rb
@@ -17,7 +17,7 @@ class Api::V1::WechatController < Api::V1::BaseController
     open(tmp_file, "wb") do |file|
       file.print open(url).read
     end
-    image = ActionDispatch::Http::UploadedFile.new(tempfile: tmp_file)
+    image = File.new(tmp_file)
     if team.update_attributes(:logo => image)
       render json: { logo: team.logo.url }
     else
