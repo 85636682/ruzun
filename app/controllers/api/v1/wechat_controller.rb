ba7815ceab0d4a72100e0b4ddba8b0c40541cdf6
dongqilai <85636682@qq.com>
2016-07-27 16:27:13 +0800

    upload

--- app/controllers/api/v1/wechat_controller.rb
+++ app/controllers/api/v1/wechat_controller.rb
@@ -9,13 +9,23 @@ class Api::V1::WechatController < Api::V1::BaseController
 
   def get_team_logo_from_wechat
     team = Team.find(params[:team_id])
-    res = @wechat_client.download_media_url(params[:media_id])
-    Rails.logger.info(res)
-    if team.update_attributes(:logo => res)
-      render json: { logo: team.logo.url }
+    url = @wechat_client.download_media_url(params[:media_id])
+    time = Time.now.to_i.to_s
+    tmp_file = IO.sysopen("tmp/" + time + "_img.jpg", "wb+")
+    tmp_img = IO.new(tmp_file, "wb")
+    tmp_img.write open(URI.encode(url)).read
+    if File.exist?("tmp/" + time + "_img.jpg")
+      image = ActionDispatch::Http::UploadedFile.new(:tempfile => tmp_img, :filename => File.basename(tmp_img))
+      if team.update_attributes(:logo => image)
+        render json: { logo: team.logo.url }
+      else
+        api_error(message: "上传失败！", status: 400)
+      end
+      File.delete("tmp/" + time + "_img.jpg")
     else
       api_error(message: "上传失败！", status: 400)
     end
+    
   end
 
   def get_user_avatar_from_wechat
