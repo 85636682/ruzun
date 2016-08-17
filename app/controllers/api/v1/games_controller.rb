77a3afd02564d853bc56005a0906475303b9b217
dongqilai <85636682@qq.com>
2016-08-17 19:06:50 +0800

    球员总得分排名

--- app/controllers/api/v1/games_controller.rb
+++ app/controllers/api/v1/games_controller.rb
@@ -5,6 +5,6 @@ class Api::V1::GamesController < Api::V1::BaseController
 
   def show
     @game = Game.find(params[:id])
-    @the_top_ten_users = @game.techstats.joins(:user).select("users.name as user_name, users.avatar as user_avatar, sum(pts) as pts_in_game").group("users.name").order("pts_in_game DESC").limit(10)
+    @the_top_ten_users = @game.techstats.joins(:user).select("users.name as user_name, users.avatar as user_avatar, sum(pts) as pts_in_game").group("users.name, users.avatar").order("pts_in_game DESC").limit(10)
   end
 end
