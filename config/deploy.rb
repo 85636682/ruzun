c51803439d280a7dc0f57dd819aa70f7e376b1ff
dongqilai <85636682@qq.com>
2017-02-10 13:16:23 +0800

    去除敏感文件

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,18 +1,23 @@
-
 lock "3.7.2"
 
-set :application, "ruzun"
+set :application, "dql_sports"
+set :deploy_user, "stephen"
+
 set :repo_url, "git@git.coding.net:dongqilai/ruzun-web.git"
 
-set :deploy_to, "/opt/app/ruzun"
+set :deploy_to, "/home/#{fetch(:deploy_user)}/opt/apps/#{fetch(:application)}"
 
-set :user, 'root'
-set :rbenv_type, :user
-
+set :rbenv_type, :deploy_user
+set :rbenv_ruby, '2.3.3'
 set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
 set :rbenv_map_bins, %w{rake gem bundle ruby rails}
 
 set :keep_releases, 3
 
-set :linked_files, %w{config/database.yml}
-set :linked_dirs, %w{tmp/pids tmp/sockets log}
+append :linked_files, "config/database.yml", "config/secrets.yml"
+
+append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
+
+namespace :deploy do
+  after :finishing, 'deploy:cleanup'
+end
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,5 +1,1 @@
-role :app, %w{139.224.187.7}
-role :web, %w{139.224.187.7}
-role :db,  %w{139.224.187.7}
-
-server '139.224.187.7', user: 'root', roles: %w{web app}, password: '7oL-6Zh-Dzw-ZwR'
+server "139.224.187.7", user: "stephen", roles: %w{app db web}
