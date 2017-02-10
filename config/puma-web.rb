0299602aa23f9ff6407baab99145d5d2e6dbf97a
dongqilai <85636682@qq.com>
2017-02-10 13:33:06 +0800

    升级5.0.1

---
+++ config/puma-web.rb
@@ -0,0 +1,20 @@
+shared_dir = '/opt/app/quanjiao/shared'
+pidfile "#{shared_dir}/tmp/pids/puma.pid"
+state_path "#{shared_dir}/tmp/pids/puma.state"
+stdout_redirect "#{shared_dir}/log/puma.stdout.log", "#{shared_dir}/log/puma.stderr.log", true
+bind "unix:/#{shared_dir}/tmp/sockets/puma.sock"
+daemonize true
+port 7000
+workers 4
+threads 8, 16
+preload_app!
+
+on_worker_boot do
+  ActiveSupport.on_load(:active_record) do
+    ActiveRecord::Base.establish_connection
+  end
+end
+
+before_fork do
+  ActiveRecord::Base.connection_pool.disconnect!
+end
