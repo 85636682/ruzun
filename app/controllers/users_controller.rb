42a7e8133a4f4251a66aba37c3a8f3389aa48507
dongqilai <85636682@qq.com>
2017-04-29 09:26:25 +0800

    微信授权

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,6 @@ class UsersController < ApplicationController
 
   def omniauth_callbacks
     response = HTTP.get("https://api.weixin.qq.com/sns/oauth2/access_token?appid=#{Figaro.env.wechat_app_id}&secret=#{Figaro.env.wechat_secret}&code=#{params[:code]}&grant_type=authorization_code")
-    puts response
+    Rails.logger.info response
   end
 end
