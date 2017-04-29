f608cc37e9ae39f8fa4a5a704e33d1d7c4734687
dongqilai <85636682@qq.com>
2017-04-29 14:35:23 +0800

    微信授权

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,14 +5,18 @@ class UsersController < ApplicationController
 
   def omniauth_callbacks
     @user = User.from_omniauth(request.env['omniauth.auth'])
-    # 是否是已经存在的纪录
+    
+    Rails.logger.info @user
+    Rails.logger.info session[:user_id]
+    Rails.logger.info @user.persisted?
+    # 是否是新增cord
     if @user.persisted?
       session[:user_id] = @user.id
-      redirect_to session[:current_url] ||= profile_m_users_path
+      # redirect_to profile_m_users_path
     else
       @user.save
       session[:user_id] = @user.id
-      redirect_to session[:current_url] ||= profile_m_users_path
+      # redirect_to profile_m_users_path
     end
   end
 end
