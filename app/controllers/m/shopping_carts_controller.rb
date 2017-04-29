02ca70853873cf335a3ce5f011525e72baa8d325
dongqilai <85636682@qq.com>
2017-04-29 15:09:50 +0800

    买票

--- app/controllers/m/shopping_carts_controller.rb
+++ app/controllers/m/shopping_carts_controller.rb
@@ -7,11 +7,7 @@ class M::ShoppingCartsController < MobileController
     @shopping_cart = ShoppingCart.new
     @shopping_cart.user_id = current_user.id
     @shopping_cart.ticket_id = params[:ticket_id]
-    if @shopping_cart.save
-      rediect_to m_tickets_path, notice: "添加成功."
-    else
-      rediect_to m_tickets_path, notice: "添加失败."
-    end
+    @result = @shopping_cart.save
   end
   
   def udpate
