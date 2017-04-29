f99bb2f48f7c112606c632e18a8e469a3786ca74
dongqilai <85636682@qq.com>
2017-04-29 14:48:27 +0800

    微信授权

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
       session[:user_id] = @user.id
       redirect_to session[:current_url] ||= profile_m_users_path
     else
-      @user.password = rand(999999)
+      @user.password = rand(999999).to_s
       @user.save!
       session[:user_id] = @user.id
       redirect_to session[:current_url] ||= profile_m_users_path
