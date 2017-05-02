3818ed2481de72a3d9adf2182b0802b3de52045f
dongqilai <85636682@qq.com>
2017-05-02 19:32:25 +0800

    修复bug

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
   extend Enumerize  #pending 新建状态  cooking烹饪状态  finished完成状态  canceled取消
   enumerize :position,     in: [:PG, :SG, :SF, :PF, :C], default: :SF
 
-  enum sex: [:female, :male]
+  enum sex: [:unknown, :female, :male]
 
   mount_uploader :avatar, UserAvatarUploader
   attr_accessor :uploader_secure_token
