8190bc69533fa48006c3c3bf7da65eb5c92143bf
dongqilai <85636682@qq.com>
2017-07-12 22:55:11 +0800

    导入数据

--- db/migrate/20160620092004_create_users.rb
+++ db/migrate/20160620092004_create_users.rb
@@ -3,16 +3,7 @@ class CreateUsers < ActiveRecord::Migration
     create_table :users do |t|
       t.string :mobile
       t.string :name
-      t.date   :birthday
-      t.integer :height
-      t.integer :weight
-      t.string :position
-      t.string :avatar
-      t.string :province
-      t.string :city
-      t.string :district
       t.string :password_digest
-      t.integer :team_id
 
       t.timestamps null: false
     end
