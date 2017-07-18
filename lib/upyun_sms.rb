b513dd7dfaa43b64ae5a488b8238416d8c955f7a
dongqilai <85636682@qq.com>
2017-07-18 17:06:02 +0800

    发送短信

--- lib/upyun_sms.rb
+++ lib/upyun_sms.rb
@@ -4,18 +4,17 @@ module UpyunSMS
   extend self
 
   def to(template_id, mobile, vars = "")
-    Rails.logger.info("-----发送#{mobile}短信中-----")
-    #response = HTTP.post(
-    #                'https://sms-api.upyun.com/api/messages', 
-    #                form: {
-    #                  Authorization: Figaro.env.upyun_sms_token,
-    #                  {
-    #                    "template_id": template_id,
-    #                    "mobile": mobile,
-    #                    "vars": vars
-    #                  }
-    #                }
-    #              )
+    response = HTTP.post(
+                    'https://sms-api.upyun.com/api/messages', 
+                    form: {
+                      Authorization: Figaro.env.upyun_sms_token,
+                      {
+                        "template_id": template_id,
+                        "mobile": mobile,
+                        "vars": vars
+                      }
+                    }
+                  )
   end
     
 end
